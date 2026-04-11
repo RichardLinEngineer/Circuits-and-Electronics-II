b2c9f603ffc43df78890d2f064a0a82700de9688
Richard Lin <tut03727@temple.edu>
2026-04-11 14:18:05 -0400

    N-Channel MOSFET

--- Week 10/N-Channel MOSFET.py
+++ Week 10/N-Channel MOSFET.py
@@ -16,7 +16,7 @@ def MOSFET_status(V_GS, V_th):
 
 def main(argv):
     if len(sys.argv) < 5:
-        print("Usage: python N -Channel MOSFET.py <V_DD value in V> <V_GS value in V> <V_th value in V> <R value in ohms>")
+        print("Usage: python N-Channel MOSFET.py <V_DD value in V> <V_GS value in V> <V_th value in V> <R value in ohms>")
         return
 
     V_DD = float(argv[1])
